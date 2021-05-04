336ce507966eddd817cc27114d1d00a4e765524e
GitHub-star-arch <63858220+GitHub-star-arch@users.noreply.github.com>
2021-05-04 12:53:39 +0530

    Add files via upload

---
+++ Screens/Buy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class Buy extends React.Component {
+    render(){
+      return (
+        <View>
+            <Text>Buy</Text>
+        </View>
+      );
+    }
+  }
